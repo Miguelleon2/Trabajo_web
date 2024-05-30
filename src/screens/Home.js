221ec7715c3e7369da034aa6a18ea4e21c265da3
Miguelleon2 <110541766+Miguelleon2@users.noreply.github.com>
2024-05-30 12:15:32 -0500

    cambio css home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,22 +3,22 @@ import AdminView from "../components/AdminView";
 import UserView from "../components/UserView";
 import { getAuth, signOut } from "firebase/auth";
 import { firebaseApp } from "../firebase/credenciales";
-import './Home.css';
+import './Home.css'; // Importar el CSS
 
 const auth = getAuth(firebaseApp);
 
 function Home({ user }) {
   return (
     <div className="home-container">
-      <header className="home-header">
+      <div className="home-header">
         <h1>Bienvenido a la App de Tenis</h1>
-        <button className="home-logout-button" onClick={() => signOut(auth)}>
-          <i className="fas fa-sign-out-alt"></i> Cerrar sesión
-        </button>
-      </header>
-      <main className="home-main">
-        {user.rol === "admin" ? <AdminView /> : <UserView user={user} />}
-      </main>
+      </div>
+      <div className="home-main">
+        {user.rol === "admin" ? <AdminView className="admin-view" /> : <UserView user={user} className="user-view" />}
+      </div>
+      <button className="home-logout-button" onClick={() => signOut(auth)}>
+        <i className="fas fa-sign-out-alt"></i> Cerrar sesión
+      </button>
     </div>
   );
 }
